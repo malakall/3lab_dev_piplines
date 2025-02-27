350d8215ad89e65a4173215f688a470737fa9b0e
Eterexx <artems1504@gmail.com>
2025-02-27 09:45:09 +0300

    тест с пейплайнами

--- src/app.py
+++ src/app.py
@@ -2,15 +2,16 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/format', methods=['POST'])
 def format_text():
     data = request.json
     text = data.get("text", "")
     mode = data.get("mode", "upper")
-    
+
     if not text:
         return jsonify({"error": "No text provided"}), 400
-    
+
     if mode == "upper":
         formatted_text = text.upper()
     elif mode == "lower":
@@ -19,8 +20,9 @@ def format_text():
         formatted_text = text.capitalize()
     else:
         return jsonify({"error": "Invalid mode"}), 400
-    
+
     return jsonify({"formatted_text": formatted_text})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,36 +1,51 @@
 import pytest
 from src.app import app  # Импортируем Flask-приложение из основного файла
 
+
 @pytest.fixture
 def client():
     """Создаем тестовый клиент Flask."""
     with app.test_client() as client:
         yield client
 
+
 def test_uppercase(client):
     response = client.post("/format", json={"text": "hello", "mode": "upper"})
     assert response.status_code == 200
     assert response.json == {"formatted_text": "HELLO"}
+
 
 def test_lowercase(client):
     response = client.post("/format", json={"text": "HELLO", "mode": "lower"})
     assert response.status_code == 200
     assert response.json == {"formatted_text": "hello"}
 
+
 def test_capitalize(client):
-    response = client.post("/format", json={"text": "hello world", "mode": "capitalize"})
+    response = client.post(
+        "/format",
+        json={
+            "text": "hello world",
+            "mode": "capitalize"})
     assert response.status_code == 200
     assert response.json == {"formatted_text": "Hello world"}
 
+
 def test_invalid_mode(client):
-    response = client.post("/format", json={"text": "hello", "mode": "unknown"})
+    response = client.post(
+        "/format",
+        json={
+            "text": "hello",
+            "mode": "unknown"})
     assert response.status_code == 400
     assert "error" in response.json
+
 
 def test_missing_text(client):
     response = client.post("/format", json={"mode": "upper"})
     assert response.status_code == 400
     assert "error" in response.json
+
 
 if __name__ == "__main__":
     pytest.main()
